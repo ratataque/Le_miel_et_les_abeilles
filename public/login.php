2eed20df5b6f576e71c3fbafa4604b85ec36d325
estrakio <walterkarl@hotmail.fr>
2023-01-16 22:19:20 +0100

    ajout de login

--- public/login.php
+++ public/login.php
@@ -1,1 +1,14 @@
-TEST
+<link rel="stylesheet" href="css/login.css">    
+
+<form>
+    <h1>Se connecter</h1>
+
+    <div class="inputs">
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Mot de passe">
+    </div>
+
+    <div align="center">
+        <button type="submit">Se connecter</button>
+    </div>
+</form>
