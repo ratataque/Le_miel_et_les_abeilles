aae69f4aa38e64f3a9da9549af49439dd94d8bb4
estrakio <walterkarl@hotmail.fr>
2023-01-17 18:22:38 +0100

    login cassé

---
+++ public/bidouille_karl.php
@@ -0,0 +1,6 @@
+<?php
+
+unset($_SESSION['faux']);
+unset($_SESSION['utilisateur']);
+unset($_SESSION);
+echo'qjkhqkjhg';
--- public/login.php
+++ public/login.php
@@ -3,25 +3,23 @@
 echo("<pre>");
 var_dump($_SESSION);
 echo "</pre>";
+echo("a = ".$a);
+//unset($_SESSION['faux']);
+//unset($_SESSION['utilisateur']);
+//unset($_SESSION);
 
 if (isset($_POST["email"])) {
     $conn = connection_sql();
-    $sql = "SELECT * FROM gestion WHERE email_gestion = '".$_POST['email']."';";
+    $sql = "SELECT * FROM gestion WHERE email_gestion = '".$_POST['email']."' and password_gestion = '".$_POST['password']."';";
     $verification = pg_fetch_assoc(pg_query($conn, $sql));
 
     if($verification){
-        $_SESSION["utilisateur"] = [];
         $utilisateur = array("email" => $verification["email_gestion"],
                             "poste" => $verification["poste_gestion"]);
-        array_push($utilisateur, $_SESSION["utilisateur"]);
+        $_SESSION["utilisateur"] = $utilisateur;
+
     }else{
-        ?>  
-        <div id="texte_centrer"> 
-            <h1 id = "texte_rouge"> 
-                Email ou mot de passe incorrect <br> la requete précédente n'as pas abouti.
-            </h1>
-        </div>
-        <?php
+        $a = 1;
     }
 
 }
@@ -31,7 +29,21 @@ if (isset($_POST["email"])) {
 <div id="div_formulaire">
     <form action="index.php" method="post" id="connexion_login">
         <h1>Se connecter</h1>
-
+        <?php
+        if(!empty($a)){
+            ?>  
+            <div id="texte_centrer"> 
+                <p id = "texte_rouge"> Email ou mot de passe incorrect <br><br></p>
+            </div>
+            <?php
+        }else{
+            ?>  
+            <div id="texte_cacher"> 
+                <p id = "texte_rouge"> Email ou mot de passe incorrect <br><br></p>
+            </div>
+            <?php
+        }
+        ?>  
         <div class="inputs">
             <input type="email" placeholder="Email" name="email" />
             <input type="password" placeholder="Mot de passe" name="password">
