02bf5115044790a678657c47d48154da1cf0a9cf
ratataque <ewanrimmely@outlook.fr>
2023-01-17 18:28:32 +0100

    fix miel template

--- public/template_miel.php
+++ public/template_miel.php
@@ -1,1 +1,10 @@
-<div class="space_titre"> Du Sale </div>
+<div class="space_titre">
+    <div class="titre_miel cacher">
+        Miel de Sapin tah les fous 
+    </div>
+</div>
+<div class="space_description">
+    <div class="description cacher_bas">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui libero laboriosam deserunt similique. Repellat, vitae obcaecati excepturi a repudiandae quas porro aliquid expedita ipsam consequuntur ratione exercitationem quasi similique eligendi.
+    </div>
+</div>
