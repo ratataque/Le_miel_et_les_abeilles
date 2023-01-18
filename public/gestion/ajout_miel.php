2790ba9fc89046b8623b705d2cdc612522629006
estrakio <walterkarl@hotmail.fr>
2023-01-18 15:25:34 +0100

    QJOUT MIEL

---
+++ public/gestion/ajout_miel.php
@@ -0,0 +1,18 @@
+<div>
+    <div class="col-4 bg-primary">
+a
+    </div>
+    <div class="col-7 bg-secondary">
+b
+    </div>
+    <div class="col-1 bg-success"></div>
+</div>
+<div>
+    <div class="col-4 bg-black">
+c
+    </div>
+    <div class="col-7 bg-danger">
+d
+    </div>
+    <div class="col-1 bg-warning"></div>
+</div>
