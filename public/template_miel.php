68305acf2b33dab3ddd366a37814355668bc4983
ratataque <ewanrimmely@outlook.fr>
2023-01-27 23:54:03 +0100

    miel working

--- public/template_miel.php
+++ public/template_miel.php
@@ -1,6 +1,37 @@
+<?php
+    $conn = pg_connect("host=db dbname=miel user=miel password=miel");
+
+    $sql = "SELECT * FROM miel;";
+    $miel = pg_fetch_all(pg_query($conn, $sql));
+
+    for ($i = 0; $i < count($miel); $i++) {
+        // echo "<pre>";
+        // var_dump($miel[$i]);
+        // echo "</pre>";
+
+        echo "
+            <div class='miel%$i'>
+                <div class='space_titre'>
+                    <div class='titre_miel cacher'>
+                        Miel de Sapin tah les fous 
+                    </div>
+                </div>
+                <div class='pancarte cacher_pancarte'>
+                        <img src='./images/pancarte.png' alt=''>
+                        <div class='prix'>100$</div>
+                </div>
+                <div class='space_description'>
+                    <div class='description cacher_bas'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui libero laboriosam deserunt similique. Repellat, vitae obcaecati excepturi a repudiandae quas porro aliquid expedita ipsam consequuntur ratione exercitationem quasi similique eligendi.
+                    </div>
+                </div>
+            </div>";
+    }
+?>
+    <!-- 
 <div class="space_titre">
     <div class="titre_miel cacher">
-        Miel de Sapin tah les fous 
+        Miel de Sapin tah les fous
     </div>
 </div>
 <div class="pancarte cacher_pancarte">
@@ -11,4 +42,4 @@
     <div class="description cacher_bas">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui libero laboriosam deserunt similique. Repellat, vitae obcaecati excepturi a repudiandae quas porro aliquid expedita ipsam consequuntur ratione exercitationem quasi similique eligendi.
     </div>
-</div>
+</div> -->
