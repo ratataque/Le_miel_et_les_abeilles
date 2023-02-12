de49133ba665da9bd5f1de76567df2ecb62b3289
estrakio <walterkarl@hotmail.fr>
2023-02-13 00:29:02 +0100

    modif

--- public/gestion/bidouille_karl.php
+++ public/gestion/bidouille_karl.php
@@ -1,25 +1,24 @@
-<!-- insert into gestion (nom_gestion, prenom_gestion, email_gestion, password_gestion, poste_gestion) values ('nom', 'prenom', 'mail@mail.com', 'a', 'poste');
-insert into apiculteur (nom_societe, nom_apiculteur, prenom_apiculteur, description_apiculteur) values ('bite', 'dutrou', 'michel', 'plein de information'); -->
+<form action="/gestion/administration.php?content=bidouille_karl" method="post">
+    <div class="form-group">
+        <label>Fruits :</label>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="fruit1" value="apple">
+            <label class="form-check-label">Apple</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="fruit2" value="banana">
+            <label class="form-check-label">Banana</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="fruit3" value="cherry">
+            <label class="form-check-label">Cherry</label>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 
 <?php
-
-$conn = pg_connect("host=db dbname=miel user=miel password=miel");
-
-$test = "SELECT * FROM miel;";
-$table_apiculteur = pg_fetch_all(pg_query($conn, $test));
-//echo ("<pre>");
-//var_dump($table_apiculteur);
-//echo ("</pre>");
-
-$base64_image = $table_apiculteur[6]["lien_photo_miel"];
-$unencodedData = base64_decode($base64_image);
-
-$fp = fopen("image.png", "wb");
-fwrite($fp, $unencodedData);
-fclose($fp);
-
-// affichage de l'image
-echo '<img src="image.png" alt="Image">';
-
-
+echo ("<pre>");
+var_dump($_POST);
+echo ("</pre>");
 ?>
